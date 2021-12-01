b7566ef777909bbd81c38d6ca9a1452940043236
Xnoe <xnoe@xnoe.moe>
2021-12-01 17:05:17 +0000

    Fixed major bug in hashtable.h that caused the hashtable to not actually store any data

--- src/kernel/datatypes/hashtable.h
+++ src/kernel/datatypes/hashtable.h
@@ -5,6 +5,7 @@
 #include "linkedlist.h"
 #include "../memory.h"
 #include "maybe.h"
+#include "../screenstuff.h"
 
 namespace xnoe {
   template <class key, class value>
@@ -18,35 +19,56 @@ namespace xnoe {
     }
 
     void set(key k, value v) {
-      xnoe::linkedlist<xnoe::tuple<key, value>> list = table[xnoe::hash<key>(k) % 4096];
-      xnoe::linkedlistelem<xnoe::tuple<key, value>>* current = list.start;
+      xnoe::linkedlist<xnoe::tuple<key, value>>* list = &table[xnoe::hash<key>(k) % 4096];
+      xnoe::linkedlistelem<xnoe::tuple<key, value>>* current = list->start;
 
       bool exists = false;
 
       if (current) {
-        while (current->next) {
+        while (current) {
           if (xnoe::get<0>(current->elem) == k) {
             exists = true;
             break;
           }
         }
+        current = current->next;
       }
       
       if (exists)
         current->elem = xnoe::tuple<key, value>(k, v);
-      else
-        list.append(xnoe::tuple<key, value>(k, v));
+      else {
+        printf("Appended.\n");
+        list->append(xnoe::tuple<key, value>(k, v));
+      }
     }
 
     xnoe::Maybe<value> get(key k) {
-      xnoe::linkedlist<xnoe::tuple<key, value>> list = table[xnoe::hash<key>(k) % 4096];
-      xnoe::linkedlistelem<xnoe::tuple<key, value>>* current = list.start;
-      if (current)
-        while (current->next)
+      xnoe::linkedlist<xnoe::tuple<key, value>>* list = &table[xnoe::hash<key>(k) % 4096];
+      xnoe::linkedlistelem<xnoe::tuple<key, value>>* current = list->start;
+      if (current) {
+        while (current) {
           if (xnoe::get<0>(current->elem) == k)
             return xnoe::Maybe<value>(xnoe::get<1>(current->elem));
+          
+          current = current->next;
+        }
+      }
       
       return xnoe::Maybe<value>();
+    }
+
+    void remove(key k, value v) {
+      xnoe::linkedlist<xnoe::tuple<key, value>>* list = &table[xnoe::hash<key>(k) % 4096];
+      xnoe::linkedlistelem<xnoe::tuple<key, value>>* current = list->start;
+      if (current) {
+        while (current) {
+          if (xnoe::get<0>(current->elem) == k) {
+            list->remove(current);
+            delete current;
+          }
+          current = current->next;
+        }
+      }
     }
   };
 }
